a71af546242bfcd03a69fe5d1694d70da780a25e
Devank18 <67155685+Devank18@users.noreply.github.com>
2020-09-30 23:38:43 +0530

    Add files via upload

--- p5.play-boilerplate-master/p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master/p5.play-boilerplate-master/sketch.js
@@ -1,55 +1,70 @@
-
-var bullet,wall;
-var speed,weight;
-var thickness;
+var wall, thickness;
+var bullet,speed, weight;
 
 function setup() {
-  createCanvas(1600,400);
-  speed=random(233,321)
+  createCanvas(1600, 400);
+
+  speed=random(223,321)
   weight=random(30,52)
 
 
-  bullet=createSprite(50,200,50,5);
-  bullet.shapeColor=("white")
-  bullet.velocityX =speed;
+  thickness=random(22,83)
 
 
-  wall = createSprite(1200,200,thickness,height/2);
-  wall.shapeColor=color(230,230,230)
 
+
+  
+    bullet=createSprite(50, 200, 50,5);  
+    bullet.velocityX = speed;
+    bullet.shapeColor=color(255);
+
+ 
+  	
+    
+    wall=createSprite(1200, 200, thickness, height/2);  
+
+    wall.shapeColor=color(230,230,230);
+  //wall.shapeColor=color(80,80,80)
 }
 
 
 function draw() {
-  background(0,0,0);
+  background(0);  
+  //bullet.sprite.collide(wall.sprite,calculateDeformation)
+  if(hasCollided(bullet, wall))
+  {
+  	bullet.velocityX=0;
+  	var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
 
-  thickness = random(22,83)
-  speed=random(233,321)
-  weight=random(30,52)
+  	
+	if(damage>10)
+	{
+		wall.shapeColor=color(255,0,0);
+		
+	}
 
-  function hasCollided(bullet,wall){
-    bulletRightEdge=bullet.x+bullet.width
-    wallLeftEdge=wall.x;
-    if(bulletRightEdge>=wallLeftEdge)
-    {
-      return true
-    }
-    return false;
+	
+
+	if(damage<10)
+	{
+		wall.shapeColor=color(0,255,0);
+	}
+	
   }
 
-  if(hascollided(wall,bullet)){
-    bullet.velocityX=0;
-    var damge =0.5*weight*speed*speed/(thickness*thickness*thickness);
-
-    if(damage>10){
-      wall.shapeColor(255,0,0);
-    }
-    if(damage<10)
-    wall.shapeColor(0,255,0);
-  }
-
-  wall.display();
-  bullet.display()
 
   drawSprites();
+ 
+}
+
+
+function hasCollided(lbullet, lwall)
+{
+	bulletRightEdge=lbullet.x +lbullet.width;
+	wallLeftEdge=lwall.x;
+	if (bulletRightEdge>=wallLeftEdge)
+	{
+		return true
+	}
+	return false;
 }
